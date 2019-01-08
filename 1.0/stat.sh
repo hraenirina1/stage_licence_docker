c39530875a873be75ad4b1ef07229ba606c0117b
hraenirina1 <31078318+hraenirina1@users.noreply.github.com>
2019-01-08 16:32:08 +0300

    Update stat.sh

--- 1.0/stat.sh
+++ 1.0/stat.sh
@@ -1,5 +1,5 @@
-top -bn 1 -d 1 | grep '^KiB Mem' |awk '{print $4 "\n" $6 "\n" $8}' > stat.log
-top -bn 1 -d 1 | grep '^%Cpu' |awk '{print $4 "\n" $6 "\n" $8}' >> stat.log
-df | grep '/' | awk '{print $1 "\n" $5}' >> stat.log
+top -bn 1 -d 1 | grep '^KiB Mem' |awk '{print $4 "\n" $6 "\n" $8}' > /stat.log
+top -bn 1 -d 1 | grep '^%Cpu' |awk '{print $4 "\n" $6 "\n" $8}' >> /stat.log
+df | grep '/' | awk '{print $1 "\n" $5}' >> /stat.log
 pkill -fx 'nc -k -l 1234'
-cat stat.log | nc -k -l 1234
+cat /stat.log | nc -k -l 1234
